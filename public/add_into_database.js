acf5c962f4fa2df1ebb4e2204e190bf42f1e4aa4
Zihan Yi <z4yi@ucsd.edu>
2018-07-27 18:06:47 -0700

    trivial

---
+++ public/add_into_database.js
@@ -0,0 +1,14 @@
+(function() {
+    var config = {
+        apiKey: "AIzaSyCNwQawqcisZevqPOA-HKubRkEQl-CGX0Y",
+        authDomain: "real-issue-tracker.firebaseapp.com",
+        databaseURL: "https://real-issue-tracker.firebaseio.com",
+        storageBucket: "real-issue-tracker.appspot.com"
+    };
+    firebase.initializeApp(config);
+
+
+
+
+
+}());
