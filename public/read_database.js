cdcf13f536fc3ad5bba106880fd068e156b8c357
zic079 <zic079@ucsd.edu>
2018-07-30 11:15:49 -0700

    fix sign out issue

--- public/read_database.js
+++ public/read_database.js
@@ -8,29 +8,26 @@
     firebase.initializeApp(config);
 
     //const attachment;
-    var userId = 'm7f4Z3UVn6fu1OdCsnTsLvSYh1h1';
-/*
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-            userId = user.uid;
+            userId = user.uid; 
+            firebase.database().ref('/users/' + userId).once('value').then(function(snapshot){
+                content = "";
+                snapshot.forEach(function(data){
+                    content +='<tr>';
+                    content += '<td>' + data.val().id + '</td>';
+                    content += '<td>' + data.val().name + '</td>';
+                    content += '<td>' + data.val().type + '</td>';
+                    content += '<td>' + 'open' + '</td>';
+                    content += '<td>' + data.val().importance + '</td>';
+                    content += '<td>' + '2018-07-21' + '</td>';
+                    content += '</tr>';
+                });
+                document.getElementById('issue-list-content').innerHTML = content;
+            });
         } else {
             alert("You are not logged in, how could you get to this page???");
+            return null;
         }
-    });
-*/
-
-    firebase.database().ref('/users/' + userId).once('value').then(function(snapshot){
-        content = "";
-        snapshot.forEach(function(data){
-            content +='<tr>';
-            content += '<td>' + data.val().id + '</td>';
-            content += '<td>' + data.val().name + '</td>';
-            content += '<td>' + data.val().type + '</td>';
-            content += '<td>' + 'open' + '</td>';
-            content += '<td>' + data.val().importance + '</td>';
-            content += '<td>' + '2018-07-21' + '</td>';
-            content += '</tr>';
-        });
-        document.getElementById('issue-list-content').innerHTML = content;
     });
 }());
--- public/sign_out.js
+++ public/sign_out.js
@@ -1,14 +1,4 @@
 (function() {
-    const config = {
-        apiKey: "AIzaSyCNwQawqcisZevqPOA-HKubRkEQl-CGX0Y",
-        authDomain: "real-issue-tracker.firebaseapp.com",
-        databaseURL: "https://real-issue-tracker.firebaseio.com",
-        projectId: "real-issue-tracker",
-        storageBucket: "real-issue-tracker.appspot.com",
-        messagingSenderId: "817361783935"
-    };
-    firebase.initializeApp(config);
-
     const btnSignOut = document.getElementById('sign-out-btn');
     const btnSignOut2 = document.getElementById('sign-out-btn-2');
 
