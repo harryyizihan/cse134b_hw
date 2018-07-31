129d5f32d9f88540eca41ae078812c93a3092f25
Zihan Yi <z4yi@ucsd.edu>
2018-07-31 12:27:13 -0700

    gg read from json server works

--- public/read_database.js
+++ public/read_database.js
@@ -36,6 +36,42 @@
     
     //REST
     else {
+        const url = 'http://localhost:3000/db';
 
+        window.addEventListener('DOMContentLoaded', function() {
+            getIssues();
+        });
+
+        function getIssues() {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.onload = function() {
+                renderIssues(this.responseText);
+            }
+            xhr.send();
+        }
+
+        // renderIssues - render the passed issues using template literals as opposed to template tags to show possibilities
+        // approach deeply buries in the markup though!
+        function renderIssues(issues) {
+
+            data = JSON.parse(issues);
+            let numIssues = data.count[0];
+            let content = "";
+
+            for (i = 0; i < numIssues; i++) {
+                content +='<tr>';
+                content += '<td>' + data.issues[i].id + '</td>';
+                content += '<td>' + data.issues[i].name + '</td>';
+                content += '<td>' + data.issues[i].type + '</td>';
+                content += '<td>' + 'open' + '</td>';
+                content += '<td>' + data.issues[i].importance + '</td>';
+                content += '<td>' + '2018-07-21' + '</td>';
+                content += '</tr>';
+    
+            }
+
+            document.getElementById('issue-list-content').innerHTML = content;
+        }
     }
 }());
