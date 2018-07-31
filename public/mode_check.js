65bc1a6d151dc21dfdf25697b114844ff5f00e9d
Zihan Yi <z4yi@ucsd.edu>
2018-07-30 23:44:59 -0700

    add toggle button function to get global value "mode" :100:

--- public/mode_check.js
+++ public/mode_check.js
@@ -1,9 +1,9 @@
 function modeChanged(checkboxElem) {
     if (checkboxElem.checked) {
-        global_mode = 1;
+        localStorage.setItem("mode", 1);
         alert("You will be changed to Firebase mode");
     } else {
-        global_mode = 0;
+        localStorage.setItem("mode", 0);
         alert("You will be changed to REST mode");
     }
 }
