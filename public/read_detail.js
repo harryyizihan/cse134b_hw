76c69a6f3218efdb7409af03a5bfde6e051192d0
zic079 <32692755+zic079@users.noreply.github.com>
2018-08-01 02:25:06 -0700

    attempting JSON version for detail page

--- public/read_detail.js
+++ public/read_detail.js
@@ -45,6 +45,37 @@
     
     //REST
     else {
-        alert("Whoops, function not support yet.")
+        const url = 'http://localhost:3000/db';
+
+        window.addEventListener('DOMContentLoaded', function() {
+            getIssues();
+        });
+
+        function getIssues() {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.onload = function() {
+                renderIssues(this.responseText);
+            }
+            xhr.send();
+        }
+
+        // renderIssues - render the passed issues using template literals as opposed to template tags to show possibilities
+        // approach deeply buries in the markup though!
+        function renderIssues(issues) {
+            data = JSON.parse(issues);
+
+            read_name = "<h2>Issue Name: " + data.issues[issueId].name + "</h2>";
+            document.getElementById("issue-name").innerHTML = read_name;
+
+            read_type = "<h2>Issue Type: " + data.issues[issueId].type + "</h2>";
+            document.getElementById("issue-type").innerHTML = read_type;
+
+            read_time = "<h2>Issue occurred date: <time>" + data.issues[issueId].datetime + "</time></h2>";
+            document.getElementById("issue-date").innerHTML = read_time;
+
+            read_description = "<h2>Issue Description: </h2> <p>" + data.issues[issueId].description + "/<p>";
+            document.getElementById("issue-description").innerHTML = read_description;
+        }
     }
 }());
