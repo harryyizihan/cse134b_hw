883070de29093e9bdf6eeb29b213571a1b05924a
Zihan Yi <z4yi@ucsd.edu>
2018-08-01 11:17:33 -0700

    service worker first try

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+console.log('Hello from sw.js');
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
